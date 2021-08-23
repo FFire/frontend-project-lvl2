9ca114625f75e6877769cbd8993ceef8eac92b8f
overstar <8497715@gmail.com>
2021-08-23 23:46:02 +0500

    fix: remove unused option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,6 @@ import { Command } from 'commander';
 
 const program = new Command();
 program
-  .addHelpCommand(false)
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
