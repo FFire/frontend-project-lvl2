9207a50ec7bff76ea9f91ea243458e942ea55327
overstar <8497715@gmail.com>
2021-08-28 13:05:23 +0500

    extract makeDiff

--- src/mainAction.js
+++ src/mainAction.js
@@ -2,27 +2,53 @@ import _ from 'lodash';
 import * as path from 'path';
 import * as fs from 'fs';
 
-const getAbsolutePath = (filePath) => {
-  const absolutePath = path.resolve(filePath);
-  if (!fs.existsSync(absolutePath)) throw new Error('File do not exist.');
-  return absolutePath;
+const getAbsFilePath = (filePath) => {
+  const absFilePath = path.resolve(filePath);
+  if (!fs.existsSync(absFilePath)) throw new Error('File do not exist.');
+  return absFilePath;
 };
-const getFileFromDisk = (absolutePath) => fs.readFileSync(absolutePath, 'utf8');
+
+const readFilePath = (absFilepath) => fs.readFileSync(absFilepath, 'utf8');
+
 const parseJson = (rawFile) => JSON.parse(rawFile);
+
 const getObject = (filePath) => {
-  const absolutePath = getAbsolutePath(filePath);
-  const rawFile = getFileFromDisk(absolutePath);
-  const data = parseJson(rawFile);
-  return data;
+  const absFilePath = getAbsFilePath(filePath);
+  const rawFile = readFilePath(absFilePath);
+  return parseJson(rawFile);
+};
+
+const states = {
+  CREATED: 'created',
+  UPDATED: 'updated',
+  DELETED: 'deleted',
+  UNCHANGED: 'unchanged',
+};
+
+const makeDiff = (key, value1, value2, state) => ({
+  path: [key],
+  value1,
+  value2,
+  state,
+});
+
+const objectsDiff = (obj1, obj2) => {
+  const keys1 = _.keys(obj1);
+  const keys2 = _.keys(obj2);
+  const keys = _.union(keys1, keys2).sort();
+  const state = states.CREATED;
+
+  return keys.map((key) => makeDiff(key, obj1[key], obj2[key], state));
 };
 
 const mainAction = (filepath1, filepath2, options = { format: 'text' }) => {
   const obj1 = getObject(filepath1);
   const obj2 = getObject(filepath2);
-  console.log('🚀 > rawFile1', obj1);
-  console.log('🚀 > rawFile2', obj2);
 
-  _.noop(options);
+  const diffObjects = objectsDiff(obj1, obj2);
+  console.log('🚀 > diffObject', diffObjects);
+
+  return _.noop(options);
 };
 
 export default mainAction;
