90550d8d629377c10f2203d99ef6b2a9a4d3c005
overstar <8497715@gmail.com>
2021-08-31 00:22:16 +0500

    step6 test prepared

--- __tests__/gen-diff.test.js
+++ __tests__/gen-diff.test.js
@@ -8,7 +8,6 @@ const __dirname = path.dirname(__filename);
 const fixturesPath = path.resolve(__dirname, '../__fixtures__');
 const readFilePath = (absFilepath) => fs.readFileSync(absFilepath, 'utf8');
 const expected1 = readFilePath(path.resolve(fixturesPath, 'expect1.txt'));
-const expected2 = readFilePath(path.resolve(fixturesPath, 'expect2.txt'));
 
 test('Without params', () => {
   expect(() => genDiff()).toThrow("error: missing required argument 'filepath1'");
@@ -35,10 +34,4 @@ test('YAML Absolute and relative paths', () => {
   const absFilePath2 = path.resolve(__dirname, '..', filName2);
   expect(genDiff(absFilePath1, absFilePath2)).toEqual(expected1);
   expect(genDiff(filName1, filName2)).toEqual(expected1);
-});
-
-test('only file1', () => {
-  const filName1 = 'file1.json';
-  const absFilePath1 = path.resolve(fixturesPath, filName1);
-  expect(genDiff(absFilePath1, absFilePath1)).toEqual(expected2);
 });
