e20172034ae7606d047ea9e556c1fb13652df8ef
overstar <8497715@gmail.com>
2021-09-18 17:59:12 +0500

    format done

--- src/formatters/formatStylish.js
+++ src/formatters/formatStylish.js
@@ -1,141 +1,75 @@
 // @ts-check
 
 import _ from 'lodash';
-import { testDiffs } from '../testObjects.js';
 import states from '../states.js';
+// import { testDiffs } from '../testObjects.js';
 
-const getTabs = (depth) => {
-  const tab = '    ';
-  return `${tab.repeat(depth)}`;
+const textState = (state) => {
+  switch (state) {
+    case states.CREATED: return '  + ';
+    case states.DELETED: return '  - ';
+    case states.UNCHANGED: return '    ';
+    case states.CHANGED: return '  - ';
+    case states.KEY: return '    ';
+
+    default: return '';
+  }
 };
 
-const getTextState = (state) => {
-  const textState = {};
-  textState[states.CREATED] = '+';
-  textState[states.DELETED] = '-';
-  textState[states.UNCHANGED] = ' ';
-  textState[states.CHANGED] = '-';
-  textState[states.KEY] = ' ';
-  return textState[state];
-};
+const getTabs = (depth) => '    '.repeat(depth);
 
-// const signs = makeSigns();
+const renderValue = (value, depth = 0) => {
+  if (!_.isObject(value)) return String(value);
 
-// const makeCloseString = (depth) => {
-//   const closeBracket = '}';
-//   const leftMargin = depth * tabSize + 1;
-//   return `${_.padStart(closeBracket, leftMargin)}`;
-// };
+  const lines = _.keys(value).reduce((acc, key) => {
+    acc.push([
+      getTabs(depth + 1), key, ': ', renderValue(value[key], depth + 1),
+    ].join(''));
 
-// const makeParentStr = (state, propName, depth, drawSign) => {
-//   const openBracket = '{';
-//   const sign = (drawSign === 'no')
-//     ? signs[states.UNCHANGED]
-//     : signs[state];
-//   const leftMargin = depth * tabSize - 1;
-//   const signStr = (depth > 0) ? `${_.padStart(sign, leftMargin)} ` : '';
-//   const propNameStr = (depth > 0) ? `${propName}: ` : '';
-//   return `${signStr}${propNameStr}${openBracket}`;
-// };
-
-// const makeChildStr = (state, propName, values, depth, drawSign) => {
-//   const makeSignStr = (currState) => {
-//     const sign = (drawSign === 'no')
-//       ? signs[states.UNCHANGED]
-//       : signs[currState];
-//     const leftMargin = depth * tabSize - 1;
-//     const signStr = `${_.padStart(sign, leftMargin)} `;
-//     return signStr;
-//   };
-
-//   const formatString = (sign, property, value) => {
-//     _.noop();
-//     return `${sign}${property}: ${value}`;
-//   };
-
-//   const acc = [];
-//   const [value1, value2] = values;
-//   if (state === states.CREATED || state === states.UNCHANGED) {
-//     const signStr = makeSignStr(state);
-//     acc.push(formatString(signStr, propName, value2));
-//   }
-//   if ((state === states.DELETED || state === states.CHANGED) && !df.isObject(value1)) {
-//     const signStr = makeSignStr(states.DELETED);
-//     acc.push(formatString(signStr, propName, value1));
-//   }
-//   if (state === states.CHANGED) {
-//     const signStr = makeSignStr(states.CREATED);
-//     acc.push(formatString(signStr, propName, value2));
-//   }
-
-//   return acc;
-// };
-// {
-//   common: {
-//     + follow: false
-//       setting1: Value 1
-//     - setting2: 200
-//     - setting3: true
-//     + setting3: null
-//     + setting4: blah blah
-//     + setting5: {
-//           key5: value5
-//       }
-//       setting6: {
-//           doge: {
-//             - wow:
-//             + wow: so much
-//           }
-//           key: value
-//         + ops: vops
-//       }
-//   }
-//   group1: {
-//     - baz: bas
-//     + baz: bars
-//       foo: bar
-//     - nest: {
-//           key: value
-//       }
-//     + nest: str
-//   }
-// - group2: {
-//       abc: 12345
-//       deep: {
-//           id: 45
-//       }
-//   }
-// + group3: {
-//       deep: {
-//           id: {
-//               number: 45
-//           }
-//       }
-//       fee: 100500
-//   }
-// }
-const formatStylish = (diffs) => {
-  const drawLines = (diff, depth = 0) => {
-    const acc = diff.map((item) => {
-      const { property, state, value } = item;
-      _.noop();
-      console.log(item);
-      if (state === states.CREATED || state === states.DELETED || state === states.UNCHANGED) {
-        return `${getTabs(depth)}${getTextState(state)} ${property}: ${value}`;
-      }
-      if (state === states.CHANGED) {
-        return `${getTabs(depth)}${getTextState(states.DELETED)} ${property}: ${value}\n${getTabs(depth)}${getTextState(states.CREATED)} ${property}: ${value}`;
-      }
-      if (state === states.KEY) {
-        return `${getTabs(depth)}${getTextState(state)} ${property}: ${drawLines(value, depth + 1)}`;
-      }
-
-      return '';
-    });
     return acc;
-  };
-  return drawLines(diffs);
+  }, []);
+
+  return `{\n${lines.join('\n')}\n${getTabs(depth)}}`;
+};
+
+const renderCommon = (depth, state, property, value) => [
+  '\n', getTabs(depth), textState(state), property,
+  ': ', renderValue(value, depth + 1),
+].join('');
+
+const renderChanged = (depth, property, oldValue, newValue) => [
+  '\n', getTabs(depth), textState(states.DELETED), property,
+  ': ', renderValue(oldValue, depth + 1),
+  '\n', getTabs(depth), textState(states.CREATED), property,
+  ': ', renderValue(newValue, depth + 1),
+].join('');
+
+const renderKey = (depth, property, values) => [
+  '\n', getTabs(depth), property,
+  ': {', ...values, '\n', getTabs(depth), '}',
+].join('');
+
+const formatStylish = (diffs) => {
+  const renderProps = (diff, depth = 0) => diff.reduce((acc, item) => {
+    const {
+      property, state, value, oldValue, newValue,
+    } = item;
+
+    if (state === states.CREATED || state === states.DELETED || state === states.UNCHANGED) {
+      acc.push(renderCommon(depth, state, property, value));
+    } else if (state === states.CHANGED) {
+      acc.push(renderChanged(depth, property, oldValue, newValue));
+    } else if (state === states.KEY) {
+      acc.push(renderKey(depth + 1, property, renderProps(value, depth + 1)));
+    }
+
+    return acc;
+  }, []);
+
+  const lines = renderProps(diffs);
+  return `{${lines}\n}`;
 };
 
 export default formatStylish;
-console.log(formatStylish(testDiffs));
+// const out = formatStylish(testDiffs);
+// console.log(out);
