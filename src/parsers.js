00ffdc84e2aa9ce4c152ab2fbc76cfc8c9956b03
overstar <8497715@gmail.com>
2021-09-19 02:19:30 +0500

    fix: group switch cases

--- src/parsers.js
+++ src/parsers.js
@@ -4,9 +4,12 @@ import yaml from 'js-yaml';
 
 const parseFile = (rawFile, extName) => {
   switch (extName) {
-    case '.json': return JSON.parse(rawFile);
-    case '.yaml': return yaml.load(rawFile);
-    case '.yml': return yaml.load(rawFile);
+    case '.json':
+      return JSON.parse(rawFile);
+
+    case '.yaml':
+    case '.yml':
+      return yaml.load(rawFile);
 
     default: throw new Error(`File type is undefined: '${extName}'`);
   }
