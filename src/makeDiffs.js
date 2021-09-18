e012790d806a08e72e42fd6d0a930c857fa5e0e2
overstar <8497715@gmail.com>
2021-09-19 00:13:43 +0500

    fix: extract makeNode outside

--- src/makeDiffs.js
+++ src/makeDiffs.js
@@ -3,43 +3,31 @@
 import _ from 'lodash';
 import states from './states.js';
 
+const makeNode = (property, state, value1, value2) => {
+  if (value2 === undefined) {
+    return { property, state, value: value1 };
+  }
+  return {
+    property, state, oldValue: value1, newValue: value2,
+  };
+};
+
 const makeDiffs = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
   const result = keys.flatMap((key) => {
     if (!_.has(obj1, key)) {
-      return {
-        property: key,
-        state: states.CREATED,
-        value: obj2[key],
-      };
+      return makeNode(key, states.CREATED, obj2[key]);
     }
     if (!_.has(obj2, key)) {
-      return {
-        property: key,
-        state: states.DELETED,
-        value: obj1[key],
-      };
+      return makeNode(key, states.DELETED, obj1[key]);
     }
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      return {
-        property: key,
-        state: states.KEY,
-        value: makeDiffs(obj1[key], obj2[key]),
-      };
+      return makeNode(key, states.KEY, makeDiffs(obj1[key], obj2[key]));
     }
     if (!_.isEqual(obj1[key], obj2[key])) {
-      return {
-        property: key,
-        state: states.CHANGED,
-        oldValue: obj1[key],
-        newValue: obj2[key],
-      };
+      return makeNode(key, states.CHANGED, obj1[key], obj2[key]);
     }
-    return {
-      property: key,
-      state: states.UNCHANGED,
-      value: obj1[key],
-    };
+    return makeNode(key, states.UNCHANGED, obj1[key]);
   });
 
   return _.sortBy(result, (item) => item.property);
